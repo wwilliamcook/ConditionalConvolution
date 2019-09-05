35da17be51fd0b6d52ce3f5e14e04678f5a9ae84
Weston Cook <wwilliamcook@gmail.com>
2019-09-05 01:49:10 -0700

    Add use_bias arg to QueryableConv2D

--- layers.py
+++ layers.py
@@ -23,7 +23,7 @@ class QueryableConv2D(kl.Layer):
     as well as the addition of hidden layers.
     """
     def __init__(self, units, ksizes, strides=None, rates=None,
-                 padding='VALID', activation=None):
+                 padding='VALID', activation=None, use_bias=True):
         """Constructs a "queryable convolution" layer.
 
         Args:
@@ -50,6 +50,7 @@ class QueryableConv2D(kl.Layer):
             if len(activation) != len(units):
                 raise ValueError('hidden_units and hidden_activations must have the same length.')
         self.activations = [kl.Activation(a) for a in activation]
+        self.use_bias = use_bias
         self.ksizes = [1, ksizes[0], ksizes[1], 1]
         self.strides = strides
         self.rates = rates
@@ -66,26 +67,40 @@ class QueryableConv2D(kl.Layer):
         self.patches_d = self.ksizes[1]*self.ksizes[2]*im_d + self.queries_d
         # Create the weights and biases for each layer
         self.layer_weights = []
-        W = self.add_weight(shape=[self.patches_d, self.units[0]],
-                            initializer='random_normal',
-                            trainable=True,
-                            name='W1')
-        b = self.add_weight(shape=[self.units[0]],
-                            initializer='random_normal',
-                            trainable=True,
-                            name='b1')
-        self.layer_weights.append([W, b])
-        for i in range(1, len(self.units)):
-            W = self.add_weight(shape=[self.units[i - 1],
-                                       self.units[i]],
+        if self.use_bias:
+            W = self.add_weight(shape=[self.patches_d, self.units[0]],
                                 initializer='random_normal',
                                 trainable=True,
-                                name='W%d' % (i + 1))
-            b = self.add_weight(shape=[self.units[i]],
+                                name='W1')
+            b = self.add_weight(shape=[self.units[0]],
                                 initializer='random_normal',
                                 trainable=True,
-                                name='b%d' % (i + 1))
+                                name='b1')
             self.layer_weights.append([W, b])
+            for i in range(1, len(self.units)):
+                W = self.add_weight(shape=[self.units[i - 1],
+                                        self.units[i]],
+                                    initializer='random_normal',
+                                    trainable=True,
+                                    name='W%d' % (i + 1))
+                b = self.add_weight(shape=[self.units[i]],
+                                    initializer='random_normal',
+                                    trainable=True,
+                                    name='b%d' % (i + 1))
+                self.layer_weights.append([W, b])
+        else:
+            W = self.add_weight(shape=[self.patches_d, self.units[0]],
+                                initializer='random_normal',
+                                trainable=True,
+                                name='W1')
+            self.layer_weights.append(W)
+            for i in range(1, len(self.units)):
+                W = self.add_weight(shape=[self.units[i - 1],
+                                        self.units[i]],
+                                    initializer='random_normal',
+                                    trainable=True,
+                                    name='W%d' % (i + 1))
+                self.layer_weights.append(W)
 
     def call(self, inputs):
         images, queries = inputs  # Two input tensors
@@ -105,8 +120,13 @@ class QueryableConv2D(kl.Layer):
                      tf.shape(queries), tf.shape(patches)))
         # Augment the patches with the queries
         y = tf.concat([patches, queries], axis=3)
-        # Perform the matmul + bias + activation for each layer
-        for ((W, b), activation) in zip(self.layer_weights, self.activations):
-            y = activation(tf.einsum('bijd,de -> bije', y, W) + b)
+        if self.use_bias:
+            # Perform the matmul + bias + activation for each layer
+            for ((W, b), activation) in zip(self.layer_weights, self.activations):
+                y = activation(tf.einsum('bijd,de -> bije', y, W) + b)
+        else:
+            # Perform the matmul + activation for each layer
+            for (W, activation) in zip(self.layer_weights, self.activations):
+                y = activation(tf.einsum('bijd,de -> bije', y, W))
 
         return y
