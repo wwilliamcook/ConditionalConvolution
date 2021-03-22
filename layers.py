b524424108be3f13ff9c443440a1cdf46c6c84d2
Weston Cook <wwilliamcook@gmail.com>
2021-03-22 02:22:56 -0700

    Rename `QueriableConv2D` to `CondConv` and refine implementation.
    Changes to `QueriableConv2D`:
    * Factor out duplicate matrix multiplications to allow drastically simpler implementation
    * Change constructor arguments to match new implementation
    * Make implementation independent of number of convolution dimensions
    
    Changes to CreativeNoise:
    * Remove redundant constructor argument: standard devation
    * Improve code formatting

--- layers.py
+++ layers.py
@@ -1,137 +1,36 @@
-"""Implementation of a queryable convolutional layer in Tensorflow 2.0 Keras.
-
-Also includes some other custom layers.
+"""Implementation of the CondConv layer in Tensorflow Keras.
 """
 
 
 import tensorflow as tf
 import tensorflow.keras.layers as kl
-import numpy as np
 
 
-__author__ = "Weston Cook"
-__license__ = "Not yet licensed"
-__email__ = "wwcook@ucsc.edu"
-__status__ = "Prototype"
-__data__ = "29 Aug. 2019"
+class CondConv(kl.Layer):
+    """Wraps a Conv layer to make a conditional convolution layer.
 
-
-class QueryableConv2D(kl.Layer):
-    """Implements a "queryable convolution" layer.
-
-    A queryable convolution is identical to a standard
-    convolution except that an additional "query" vector
-    is convatenated depthwise onto the image patches tensor,
-    as well as the addition of hidden layers.
+    A conditional convolution is identical to a standard convolution except
+    that the output depends on an additional batch of vectors.
     """
-    def __init__(self, units, ksizes, strides=None, rates=None,
-                 padding='VALID', activation=None, use_bias=True):
-        """Constructs a "queryable convolution" layer.
+    def __init__(self, conv_layer, activation=None):
+        """Conditional convolution layer.
 
-        Args:
-            units: Integer specifying the depth of the output image.
-            ksize: [height, width].
-            strides: [height, width].
-            rates: [height, width].
-            padding: 'VALID' or 'SAME'.
-            activation: Activation to use for output layer.
-            hidden_units: List of integers specifying depths of hidden layers.
-            hidden_activations: List of activations to use for hidden layers.
+        :param: conv_layer: instance of any kind of keras convolution layer
+        :param: activation: activation function
         """
-        super(QueryableConv2D, self).__init__()
-        if strides is None:
-            strides = [1, 1, 1, 1]
-        if rates is None:
-            rates = [1, 1, 1, 1]
-        if np.shape(units) == ():
-            units = [units]
-        self.units = units
-        if np.shape(activation) == ():
-            activation = [activation]
-        else:
-            if len(activation) != len(units):
-                raise ValueError('hidden_units and hidden_activations must have the same length.')
-        self.activations = [kl.Activation(a) for a in activation]
-        self.use_bias = use_bias
-        self.ksizes = [1, ksizes[0], ksizes[1], 1]
-        self.strides = strides
-        self.rates = rates
-        self.padding = padding
-
-    def build(self, input_shape):
-        if len(input_shape) != 2:
-            raise ValueError('Invalid arguments: expected [images, queries].')
-        im_batch_size, im_h, im_w, im_d = input_shape[0]
-        q_batch_size, self.queries_d = input_shape[1]
-        if im_batch_size != q_batch_size:
-            raise ValueError('images and queries must have the same batch size.')
-        # Calculate the depth of the augmented patches tensor
-        self.patches_d = self.ksizes[1]*self.ksizes[2]*im_d + self.queries_d
-        # Create the weights and biases for each layer
-        self.layer_weights = []
-        if self.use_bias:
-            W = self.add_weight(shape=[self.patches_d, self.units[0]],
-                                initializer='random_normal',
-                                trainable=True,
-                                name='W1')
-            b = self.add_weight(shape=[self.units[0]],
-                                initializer='random_normal',
-                                trainable=True,
-                                name='b1')
-            self.layer_weights.append([W, b])
-            for i in range(1, len(self.units)):
-                W = self.add_weight(shape=[self.units[i - 1],
-                                        self.units[i]],
-                                    initializer='random_normal',
-                                    trainable=True,
-                                    name='W%d' % (i + 1))
-                b = self.add_weight(shape=[self.units[i]],
-                                    initializer='random_normal',
-                                    trainable=True,
-                                    name='b%d' % (i + 1))
-                self.layer_weights.append([W, b])
-        else:
-            W = self.add_weight(shape=[self.patches_d, self.units[0]],
-                                initializer='random_normal',
-                                trainable=True,
-                                name='W1')
-            self.layer_weights.append(W)
-            for i in range(1, len(self.units)):
-                W = self.add_weight(shape=[self.units[i - 1],
-                                        self.units[i]],
-                                    initializer='random_normal',
-                                    trainable=True,
-                                    name='W%d' % (i + 1))
-                self.layer_weights.append(W)
+        super(CondConv, self).__init__()
+        self._conv_layer = conv_layer
+        self._dense_layer = kl.Dense(conv_layer.filters, use_bias=False)
+        self._activation = kl.Activation(activation)
 
     def call(self, inputs):
-        images, queries = inputs  # Two input tensors
-        # Separate the image into patches for performing convolution
-        patches = tf.image.extract_patches(images=images,
-                                           sizes=self.ksizes,
-                                           strides=self.strides,
-                                           rates=self.rates,
-                                           padding=self.padding)
-        # Reshape and broadcast queries
-        #queries = tf.expand_dims(tf.expand_dims(queries, 1), 1)
-        reshape_shape = [1, 1, queries.shape[1]]
-        queries = kl.Reshape(reshape_shape)(queries)
-        queries = tf.broadcast_to(
-            queries,
-            tf.where([True, False, False, True],
-                     tf.shape(queries), tf.shape(patches)))
-        # Augment the patches with the queries
-        y = tf.concat([patches, queries], axis=3)
-        if self.use_bias:
-            # Perform the matmul + bias + activation for each layer
-            for ((W, b), activation) in zip(self.layer_weights, self.activations):
-                y = activation(tf.einsum('bijd,de -> bije', y, W) + b)
-        else:
-            # Perform the matmul + activation for each layer
-            for (W, activation) in zip(self.layer_weights, self.activations):
-                y = activation(tf.einsum('bijd,de -> bije', y, W))
+        images, vectors = inputs  # Two input tensors
+
+        y = self._conv_layer(images) + self._dense_layer(vectors)
+        y = self._activation(y)
 
         return y
+
 
 class CreativeNoise(kl.Layer):
     """Adds gaussian noise to the input.
@@ -140,23 +39,25 @@ class CreativeNoise(kl.Layer):
     by allowing the magnitude of the noise to be trained while also adding
     a loss to keep the magnitude from being trained to zero.
     """
-    def __init__(self, loss_rate=1.0, stddev=1.0):
+    def __init__(self, loss_rate=1.0):
         """Adds trainable gaussian noise to the input.
+
+        :param float: loss_rate: amount to scale nonzero-noise loss by
         """
         super(CreativeNoise, self).__init__()
-        self.loss_rate = loss_rate
-        self.stddev = stddev
-    
+        self._loss_rate = loss_rate
+
     def build(self, input_shape):
         # Add a trainable weight to multiply the noise by
-        self.noise_weights = self.add_weight(shape=input_shape,
-                                             initializer='ones',
-                                             trainable=True)
-    
+        self._stddev = self.add_weight(shape=input_shape[1:],
+                                       initializer='ones',
+                                       trainable=True)
+
     def call(self, inputs):
         # Create a loss to keep the noise from being removed completely
-        self.add_loss(-self.loss_rate * tf.reduce_sum(tf.math.log(tf.math.abs(self.noise_weights))))
-        # Add the weighted noise to the input
-        noise = tf.random.normal(tf.shape(inputs), stddev=self.stddev)
-        weighted_noise = noise * self.noise_weights
-        return inputs + weighted_noise
+        self.add_loss(-self._loss_rate * tf.reduce_sum(
+            tf.math.log(tf.math.abs(self._stddev))))
+
+        noise = tf.random.normal(tf.shape(inputs)) * self._stddev
+
+        return inputs + noise
